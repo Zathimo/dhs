aec8dc2ad01c519bc6a9ac6b3cdb94b7dcb0cc55
451mov <patrick.carson01@gmail.com>
2021-01-05 00:16:01 +0000

    Update download_landsat.py

--- download_landsat.py
+++ download_landsat.py
@@ -9,9 +9,9 @@ parser.add_argument('--city_name', dest='city_name', action='store',
 parser.add_argument('--data_path', dest='data_path', action='store',
                     help='absolute path to metadata.csv')
 parser.add_argument('--start_date', dest='start_date', action='store',
-                    help='start date for filtering a collection by a date range')
+                    help='start date for filtering ee.ImageCollection by date range')
 parser.add_argument('--end_date', dest='end_date', action='store',
-                    help='end date for filtering a collection by a date range')
+                    help='end date for filtering ee.ImageCollection by date range')
 
 args = parser.parse_args()
 
