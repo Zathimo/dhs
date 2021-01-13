e08bd3db4aa59cf64def1f3d67b2e2050841e325
Patrick Carson <patrick.carson01@gmail.com>
2021-01-13 23:38:00 +0000

    - given a lat and long create an area of interest for survey clusters

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,17 @@
 from functools import partial
 
 import pyproj
+from shapely.geometry import Point
 from shapely.ops import transform
+
+
+class GoogleEarthEngineDownloadError(Exception):
+    """Indicates an error occured whilst downloading image from GEE"""
+    def __init__(self, message):
+        self.error_message = message
+
+    def __str__(self):
+        return f'gee>> download failed! message: {self.error_message}'
 
 
 def reproject_geom(geom, src_epsg, dst_epsg):
@@ -12,3 +22,16 @@ def reproject_geom(geom, src_epsg, dst_epsg):
     dst_proj = pyproj.Proj(init='epsg:{}'.format(dst_epsg))
     reproj = partial(pyproj.transform, src_proj, dst_proj)
     return transform(reproj, geom)
+
+
+def area_of_interest(lat, lon, km):
+    proj_wgs84 = pyproj.Proj('+proj=longlat +datum=WGS84')
+    aeqd_proj = '+proj=aeqd +lat_0={lat} +lon_0={lon} +x_0=0 +y_0=0'
+    project = partial(
+        pyproj.transform,
+        pyproj.Proj(aeqd_proj.format(lat=lat, lon=lon)),
+        proj_wgs84)
+    buf = Point(0, 0).buffer(km * 1000)
+    aoi = transform(project, buf).exterior.envelope
+    xmin, ymin, xmax, ymax = [round(coord, 3) for coord in aoi.bounds]
+    return xmin, ymin, xmax, ymax
