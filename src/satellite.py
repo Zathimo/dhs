3e87bb495d63cdbab4b8059cf822c6c5e1b2f93c
Patrick Carson <patrick.carson01@gmail.com>
2021-01-14 00:13:41 +0000

    - add nightlight bands, long/lat band

--- src/satellite.py
+++ src/satellite.py
@@ -6,56 +6,49 @@ ee.Initialize()
 
 
 class Landsat:
-    def __init__(self, city, start_date, end_date):
+    def __init__(self, aoi_coords, start_date, end_date):
         """
         Constructor.
 
         Parameters
         ----------
-        :param city: Object
-            city-level metadata
+        :param aoi_coords: list
+            coordinates for area of interest; [xmin, ymin, xmax, ymax]
         :param start_date: str
             start date for filtering a collection by a date range
         :param end_date: str
             end date for filtering a collection by a date range
         """
-        self.city = city
+        self.aoi_coords = aoi_coords
         self.start_date = start_date
         self.end_date = end_date
+        self.rect = ee.Geometry.Rectangle(self.aoi_coords)
+        self.coords_json = self.rect.getInfo()['coordinates']
         self.ms_bands = ['BLUE', 'GREEN', 'RED', 'NIR', 'SWIR1', 'SWIR2', 'TEMP1', 'TEMP2']
-        self.stack = (self.collection('LANDSAT/LC08/C01/T1_SR')
-                      .map(self.rename_l8)
-                      .map(self.rescale_l8)
-                      .map(self.mask_qaclear)
-                      .select(self.ms_bands)
-                      .median())
 
-    def collection(self, name):
+    @staticmethod
+    def add_latlon(image):
         """
-        Creates an ee.ImageCollection stack or sequence of images
-        for aoi between the desired start and end dates.
+        :param image: ee.Image
+            Landsat8 satellite image
 
-        Args:
-            :param name: str
-                name of image collection
-        Returns:
-            :returns iee.ImageCollection:
-                an ImageCollection stack or sequence of images
+        :returns masked_image: ee.Image
+            add bands of longitude and latitude
+            coordinates named 'LON' and 'LAT'
         """
-        return (ee.ImageCollection(name)
-                .filterBounds(ee.Geometry.Rectangle(self.city.bbox))
-                .filterDate(self.start_date, self.end_date))
+        latlon = image.pixelLonLat().select(
+            opt_selectors=['longitude', 'latitude'],
+            opt_names=['LON', 'LAT'])
+        return image.addBands(latlon)
 
     @staticmethod
     def rename_l8(image):
         """
-        Args:
-            :param image: ee.Image
-                landsat8 image
+        :param image: ee.Image
+            Landsat8 image
 
-        Returns:
-            :returns:
-                ee.Image with bands rescaled
+        :returns:
+            ee.Image with bands rescaled
         """
         newnames = ['AEROS', 'BLUE', 'GREEN', 'RED', 'NIR', 'SWIR1', 'SWIR2',
                     'TEMP1', 'TEMP2', 'sr_aerosol', 'pixel_qa', 'radsat_qa']
@@ -64,11 +57,10 @@ class Landsat:
     @staticmethod
     def rescale_l8(image):
         """
-        Args:
-            :param image: ee.Image
-                landsat8 image
-        Returns:
-            :returns scaled:
+        :param image: ee.Image
+                Landsat8 image
+
+        :returns scaled:
                 ee.Image with bands rescaled
         """
         opt = image.select(['AEROS', 'BLUE', 'GREEN', 'RED', 'NIR', 'SWIR1', 'SWIR2'])
@@ -86,21 +78,19 @@ class Landsat:
     @staticmethod
     def decode_qamask(image):
         """
-        Args:
-            :param image: ee.Image
-                Landsat satellite image containing 'pixel_qa' band
+        :param image: ee.Image
+            Landsat8 satellite image containing 'pixel_qa' band
 
-        Returns:
-            :returns masks: ee.Image
-                contains 5 bands of masks
-                Pixel QA Bit Flags
-                Bit  Attribute
-                0    Fill
-                1    Clear
-                2    Water
-                3    Cloud Shadow
-                4    Snow
-                5    Cloud
+        :returns masks: ee.Image
+            contains 5 bands of masks
+            Pixel QA Bit Flags
+            Bit  Attribute
+            0    Fill
+            1    Clear
+            2    Water
+            3    Cloud Shadow
+            4    Snow
+            5    Cloud
         """
         qa = image.select('pixel_qa')
         clear = qa.bitwiseAnd(2).neq(0)  # 0 = not clear, 1 = clear
@@ -123,14 +113,12 @@ class Landsat:
 
     def mask_qaclear(self, image):
         """
-        Args:
-            :param image: ee.Image
-                Landsat satellite image
+        :param image: ee.Image
+            Landsat8 satellite image
 
-        Returns:
-            :returns masked_image: ee.Image
-                input image with cloud-shadow, snow, cloud, and unclear
-                pixels masked out
+        :returns masked_image: ee.Image
+            input image with cloud-shadow, snow, cloud, and unclear
+            pixels masked out
         """
         qam = self.decode_qamask(image)
         cloudshadow_mask = qam.select('pxqa_cloudshadow')
@@ -142,21 +130,59 @@ class Landsat:
                         .updateMask(cloud_mask))
         return masked_image
 
+    @property
+    def composite_nl(self):
+        """Creates a median-composite nightlights (NL) image."""
+
+        return (ee.ImageCollection('NOAA/VIIRS/DNB/MONTHLY_V1/VCMSLCFG')
+                .filterDate(self.start_date, self.end_date)
+                .map(lambda x: x.clip(self.rect))
+                .median()
+                .select([0], ['NIGHTLIGHTS']))
+
+    @property
+    def ms_collection(self):
+        """
+        Creates an ee.ImageCollection stack or sequence of images
+        for aoi between the desired start and end dates.
+        """
+        return (ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
+                .filterDate(self.start_date, self.end_date)
+                .map(lambda x: x.clip(self.rect)))
+
+    @property
+    def image(self):
+        """
+        Creates an renamed, rescaled ee.Image with added lon, lat and nightlight bands .
+        """
+        ms_stack = (self.ms_collection
+                    .map(self.rename_l8)
+                    .map(self.rescale_l8)
+                    .map(self.mask_qaclear)
+                    .select(self.ms_bands)
+                    .median())
+        ms_stack = self.add_latlon(ms_stack)
+        ms_stack = ms_stack.addBands(self.composite_nl).toFloat()
+        return ms_stack
+
     def export_image(self):
         """
         Creates a batch task to export an Image as a raster to Google Drive.
         """
         task_config = {
-            'description': f'{self.city.name}_{self.start_date}_{self.end_date}_1',
+            'description': f'xxx_{self.start_date}_{self.end_date}',
             'scale': 30,
-            'folder': self.city.name,
-            'region': ee.Geometry.Rectangle(self.city.bbox).getInfo()['coordinates']
+            'folder': 'rwanda',
+            'region': self.coords_json
         }
-        task = ee.batch.Export.image.toDrive(self.stack, **task_config)
+        task = ee.batch.Export.image.toDrive(self.image, **task_config)
         task.start()
-        print(f'>beginning download of landsat image for {self.city.name}')
+        print(f'>beginning download of landsat image for xxx')
         while task.status()['state'] != 'COMPLETED':
             print(f">>task status: {task.status()['state']}")
+            if task.status()['state'] == 'FAILED':
+                print(task.status())
+                break
             time.sleep(3)
         print(f">>task status: {task.status()['state']}")
-        print(f'>successfully downloaded data for {self.city.name}, check your Google Drive')
+        print(f'>successfully downloaded data for xxx, check your Google Drive')
