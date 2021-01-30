6e7c9f17ff922dcb552017a7d964fff03e2203a9
451mov <patrick.carson01@gmail.com>
2021-01-30 21:46:57 +0000

    Update process_dhs.py

--- process_dhs.py
+++ process_dhs.py
@@ -6,11 +6,12 @@ import geopandas as gpd
 
 from src.utils import area_of_interest
 
-# this script takes both a DHS survey .DTA and .shp files as input.
-# It selects the cluster id and associated wealth index information.
-# This information is then merged with the latitude and longitude of
-# each cluster. An area of interest (10x10km) is then generated from
-# the latitude and longitude for each cluster.
+# this script takes correpsonding DHS survey .DTA and .shp files as input.
+# It selects out the cluster id and associated wealth index information 
+# from the .DTA file and merges it with the latitude and longitde of each 
+# cluster, found in the .shp file. An area of interest (10x10km) is then
+# generated from the coordinates of each cluster. This AOI will be used to
+# query GEE for accompanying satelite imagery.
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--country', dest='country', action='store',
@@ -29,14 +30,13 @@ if not os.path.exists(output_path):
 
 # process DHS survey file, extract mean wealth index for each cluster
 df_wealth = pd.read_stata(args.dhs_survey, convert_categoricals=False)
-df_wealth = (df_wealth[['hv001', 'hv271']]
+df_wealth = (df_wealth[['hv001', 'hv270', 'hv271']]
              .rename(columns={'hv001': 'cluster_id',
                               'hv270': 'wealth_category',
                               'hv271': 'wealth_index'})
              .dropna())
 df_wealth['wealth_index'] = df_wealth['wealth_index'] / 100000.0
 df_cluster_wealth = df_wealth.groupby('cluster_id').mean().reset_index()
-
 print('extracted and calculated mean wealth index for each cluster')
 
 # process DHS shapefile, extract cluster long, lat
@@ -49,10 +49,8 @@ df_geo = (gpd.read_file(args.dhs_gps)[['DHSCLUST', 'LATNUM', 'LONGNUM']]
 # calculate area of interest coordinates
 output = pd.merge(df_cluster_wealth, df_geo, on='cluster_id', how='inner')
 output['area_of_interest'] = output.apply(lambda x: area_of_interest(x['latitude'], x['longitude'], 5), axis=1)
-
 print('generated bounding box area of interest around each cluster')
 
 output_dest = os.path.join(output_path, f'{args.country}_cluster_wealth.csv')
 output.to_csv(output_dest, index=False)
-
 print('successfully processed DHS information')
