4e35f52c1e275b7fd5aad499e7baf1da337608c3
Isabelle Mornard <mornardisabelle@gmail.com>
2024-10-30 15:30:06 +0100

    Landsat: no error but no result

--- src/test_mosaic.py
+++ src/test_mosaic.py
@@ -15,19 +15,19 @@ import matplotlib.pyplot as plt
 import rioxarray
 
 
-
 def main():
     # cluster = GatewayCluster
 
     t = time.time()
 
-
-    bbox = [-122.27508544921875, 47.54687159892238, -121.96128845214844, 47.745787772920934]
+    bbox = (-122.27508544921875, 47.54687159892238, -121.96128845214844, 47.745787772920934)
 
     stac = pystac_client.Client.open(
         "https://planetarycomputer.microsoft.com/api/stac/v1",
         modifier=planetary_computer.sign_inplace,
     )
+
+    # Sentinel-2
 
     search = stac.search(
         bbox=bbox,
@@ -42,44 +42,68 @@ def main():
     data = (
         stackstac.stack(
             items,
-            assets=["B04", "B03", "B02"],  # red, green, blue
-            bounds_latlon=[-122.27508544921875, 47.54687159892238, -121.96128845214844, 47.745787772920934],
+            assets=["B04", "B03", "B02"],
+            bounds_latlon=bbox,
             chunksize=4096,
-            resolution=100,
+            resolution=10,
         )
-        .where(lambda x: x > 0, other=np.nan)  # sentinel-2 uses 0 as nodata
         .assign_coords(band=lambda x: x.common_name.rename("band"))  # use common names
     )
-
     print(data)
+
+    fig, ax = plt.subplots(figsize=(8, 8))
 
     data = data.persist()
 
     median = data.median(dim="time").compute()
 
     image = ms.true_color(*median)  # expects red, green, blue DataArrays
-    print(type(image))
-    print("temps ecoule :", time.time() - t)
 
-    file_name = 'hello.tif'
+    ax.set_axis_off()
+    image.plot.imshow(ax=ax)
+
+    file_name = 'cloudless_sentinel.tif'
     image.transpose('band', 'y', 'x').rio.to_raster(file_name)
 
-    # Create a figure and axis
-    fig, ax = plt.subplots()
+    # Landsat
 
-    # Plot a dark square
-    square = plt.Rectangle((0.25, 0.25), 0.5, 0.5, color='black')
-    ax.add_patch(square)
+    search = stac.search(
+        bbox=bbox,
+        datetime="2020-01-01/2020-12-31",
+        collections=["landsat-c2-l2"],
+        query={"eo:cloud_cover": {"lt": 25}},
+    )
 
-    # Set the limits of the plot
-    ax.set_xlim(0, 1)
-    ax.set_ylim(0, 1)
+    items = search.item_collection()
+    print("nombre d'items :", len(items))
+
+    data = ((
+                stackstac.stack(
+                    items,
+                    assets=["red", "green", "blue"],
+                    bounds_latlon=bbox,
+                    chunksize=4096,
+                    resolution=100,
+                )
+                .where(
+                    lambda x: x > 0, other=np.nan
+                )
+            ).median(dim="time", keep_attrs=True)
+            .persist())
+
+    landsat_image = ms.true_color(*data)
+    fig, ax = plt.subplots(figsize=(8, 8))
+
+    ax.set_axis_off()
+    landsat_image.plot.imshow(x="x", y="y", rgb="band")
+    print("temps ecoule :", time.time() - t)
 
     # Display the plot
     plt.show()
 
+    file_name = 'cloudless_landsat.tif'
+    landsat_image.transpose('band', 'y', 'x').rio.to_raster(file_name)
+
 
 if __name__ == '__main__':
-
     main()
-
