c9ae602cc2adb749de047b852806ca1d371c0454
Isabelle Mornard <mornardisabelle@gmail.com>
2024-11-07 12:02:38 +0100

    correct dates

--- process_dhs.py
+++ process_dhs.py
@@ -46,9 +46,9 @@ def main(folder_path, country, year, buffer):
     # Process DHS survey file, extract mean wealth index for each cluster
     #####################################################################
 
-    df_wealth = pd.read_stata(dhs_survey, convert_categoricals=False)
-    df_wealth['country'] = country
-    df_wealth = (df_wealth[['country', 'hv007', 'hhid', 'hv001', 'hv025']]
+    df_survey = pd.read_stata(dhs_survey, convert_categoricals=False)
+    df_survey['country'] = country
+    df_survey = (df_survey[['country', 'hv007', 'hhid', 'hv001', 'hv025']]
                  .rename(columns={'hv007': 'year',
                                   'hhid': 'HHID',
                                   'hv001': 'cluster_id',
@@ -56,7 +56,10 @@ def main(folder_path, country, year, buffer):
                                   })
                  .dropna())
 
-    df_wealth['HHID'] = df_wealth['HHID'].str.strip()
+    df_survey['HHID'] = df_survey['HHID'].astype(str).str.strip()
+
+    if df_survey['year'].astype(int)[0] < 100:
+        df_survey['year'] = df_survey['year'].astype(int) + 1900
 
     print('Selected DHS columns.')
 
@@ -70,7 +73,7 @@ def main(folder_path, country, year, buffer):
                                'LONGNUM': 'lon'}))
 
     # merge cluster wealth index and location information
-    dhs_geo = pd.merge(df_wealth, df_geo, on='cluster_id', how='inner')
+    dhs_geo = pd.merge(df_survey, df_geo, on='cluster_id', how='inner')
 
     # calculate area of interest coordinates
     dhs_geo['area_of_interest'] = dhs_geo.apply(lambda x: area_of_interest(x['lat'],
@@ -78,7 +81,7 @@ def main(folder_path, country, year, buffer):
                                                                            buffer),
                                                 axis=1)
 
-    dhs_geo.to_csv(os.path.join(output_path, f'{country}_{year}_cluster_wealth.csv'), index=False, sep=';')
+    dhs_geo.to_csv(os.path.join(output_path, f'geo_survey/{country}_{year}_cluster_wealth.csv'), index=False, sep=';')
 
     print('Generated bounding box area of interest around each cluster.')
 
@@ -99,6 +102,7 @@ def main(folder_path, country, year, buffer):
 
 if __name__ == '__main__':
     buffer = 5
+    df_errors = pd.DataFrame(columns=['folder', 'subfolder', 'error'])
 
     for folder_name in os.listdir('data'):
         folder_path = os.path.join('data', folder_name)
@@ -109,4 +113,6 @@ if __name__ == '__main__':
                 except Exception as e:
                     print(f'Error processing {folder_path}/{subfolder_name}: {e}')
                     continue
+
+    df_errors.to_csv('data/errors.csv', index=False)
 
