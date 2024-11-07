82b04587dbd93910d7482b32ce28f773862735a2
Isabelle Mornard <mornardisabelle@gmail.com>
2024-11-07 16:59:56 +0100

    read data from sustain_bench

--- src/process_IWI.py
+++ src/process_IWI.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 import matplotlib.pyplot as plt
 import scipy.stats
@@ -8,6 +9,15 @@ import os
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
+CNAMES = {'AO': 'Angola', 'BJ': 'Benin', 'BF': 'Burkina_Faso', 'BU': 'Burundi', 'CA': 'Cameroon',
+          'CF': 'Central_African_Republic', 'TD': 'Chad', 'KM': 'Comoros', 'CI': 'Cote_dIvoire',
+          'CD': 'Congo_Democratic_Republic', 'SZ': 'Eswatini', 'ET': 'Ethiopia', 'GA': 'Gabon', 'GM': 'Gambia',
+          'GH': 'Ghana', 'GN': 'Guinea', 'KE': 'Kenya', 'LS': 'Lesotho', 'LB': 'Liberia', 'MD': 'Madagascar',
+          'MW': 'Malawi', 'ML': 'Mali', 'MZ': 'Mozambique', 'NM': 'Namibia', 'NI': 'Niger',
+          'NG': 'Nigeria', 'RW': 'Rwanda', 'SL': 'Sierra_Leone', 'ZA': 'South_Africa', 'SN': 'Senegal', 'TZ': 'Tanzania',
+          'TG': 'Togo',
+          'ZM': 'Zambia', 'ZW': 'Zimbabwe'}
+
 
 def read_IWI(input_path, output_path):
     df, meta = pyreadstat.read_sav(input_path)
@@ -16,6 +26,14 @@ def read_IWI(input_path, output_path):
     output = pd.read_csv(output_path, sep=',')
 
     return output
+
+
+def read_sustain_bench():
+    df = pd.read_csv('../data/dhs_final_labels.csv')[['cname', 'year', 'cluster_id', 'lat', 'lon', 'asset_index']]
+    df = df[df['cname'].isin(CNAMES.keys())].rename(columns={'cname': 'country'})
+    df['country'] = df['country'].map(CNAMES)
+    df.rename(columns={'asset_index': 'iwi'}, inplace=True)
+    df.to_csv('../data/sustain_bench.csv', index=False)
 
 
 def get_IWI_petterson(df):
@@ -60,4 +78,4 @@ def get_IWI_global(df, input_path):
 
 
 if __name__ == "__main__":
-    pass
+    read_sustain_bench()
