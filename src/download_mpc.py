f0e229c13f68b420bb47169f382b352db47dd5cc
Isabelle Mornard <mornardisabelle@gmail.com>
2024-12-12 10:08:47 +0100

    csv file with cluster from global_data_lab_only that have been downloaded

--- src/download_mpc.py
+++ src/download_mpc.py
@@ -8,7 +8,6 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 
 def main():
-
     csv = pd.read_csv('data/areas_of_interest.csv', sep=';')
 
     df = convert_bbox_to_tuple(csv)
@@ -38,12 +37,33 @@ def main():
 
 
 def convert_bbox_to_tuple(df):
-
     # Convert the area_of_interest column to tuple of floats
     df['area_of_interest'] = df['area_of_interest'].apply(lambda x: tuple(map(float, x.strip('[]').split(','))))
 
     return df
 
 
+def extract_country_year_cluster(data_path, output_path):
+    data = []
+
+    for country_folder in os.listdir(data_path):
+        country_path = os.path.join(data_path, country_folder)
+        if os.path.isdir(country_path):
+            for year_folder in os.listdir(country_path):
+                year_path = os.path.join(country_path, year_folder)
+                if os.path.isdir(year_path):
+                    for cluster_file in os.listdir(year_path):
+                        if cluster_file.endswith('.tif'):
+                            cluster_id = int(cluster_file.split('.')[0])
+                            data.append({'country': country_folder, 'year': year_folder, 'cluster_id': cluster_id})
+
+    df = pd.DataFrame(data)
+    df.to_csv(output_path, index=False, sep=';')
+
+
 if __name__ == "__main__":
-    main()
+    aoi = pd.read_csv('../data/global_data_lab_only.csv', sep=',')
+    aoi['country'] = aoi['country'].apply(lambda x: x.lower())
+    cyc = pd.read_csv('data/downloaded_aoi.csv', sep=';')
+    filtered = pd.merge(cyc, aoi, on=['country', 'year', 'cluster_id'])
+    filtered.to_csv('data/filtered_dataset.csv', index=False, sep=';')
